e079049ad76ce7da0f113460779a8db4a921c18c
Vipin Vats <117748955+vipvats131@users.noreply.github.com>
2022-11-08 15:13:39 -0500

    Update streamlit_app.py
    Added Header Text for the items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy diner')
-streamlit.title('Starting with Breakfast Menu')
+streamlit.header('Starting with Breakfast Menu')
+streamlit.text(' Toasted Bread with Butter along with Tea')
+streamlist.text('Cereal with choice of your falvor')
+streamlit.text('Aalo Parantha with Pudina Chutney')
