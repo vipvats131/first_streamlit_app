9ad504b8837e60586bdfb7ba85faac8214986a18
Vipin Vats <117748955+vipvats131@users.noreply.github.com>
2022-11-08 15:22:06 -0500

    Update streamlit_app.py
    Added more emoji to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Starting with Breakfast Menu')
-streamlit.text('\N{bread} Toasted Bread with Butter along with Tea')
-streamlit.text('Cereal with choice of your falvor')
-streamlit.text('Aalo Parantha with Pudina Chutney')
+streamlit.text('\N{bread} \N{tea} Toasted Bread with Butter along with Tea')
+streamlit.text('\N{cereal} Cereal with choice of your falvor')
+streamlit.text('\N{flatbread} Aalo Parantha with Pudina Chutney')
