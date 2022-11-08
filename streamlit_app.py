c3e5a27ea92d40ea55ab64f85b439ab79be866ae
Vipin Vats <117748955+vipvats131@users.noreply.github.com>
2022-11-08 15:25:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Starting with Breakfast Menu')
-streamlit.text('\N{bread}  Toasted Bread with Butter along with Tea')
-streamlit.text('\N{cereal} Cereal with choice of your falvor')
+streamlit.text('\N{bread} \N{butter} \N{hot beverage} Toasted Bread with Butter along with Tea')
+streamlit.text('\N{bowl with spoon} Cereal with choice of your falvor')
 streamlit.text('\N{flatbread} Aalo Parantha with Pudina Chutney')
