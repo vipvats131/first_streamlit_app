fdef907e1f36540aef22f83ca498439b29527b21
Vipin Vats <117748955+vipvats131@users.noreply.github.com>
2022-11-08 15:14:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Starting with Breakfast Menu')
 streamlit.text(' Toasted Bread with Butter along with Tea')
-streamlist.text('Cereal with choice of your falvor')
+streamlit.text('Cereal with choice of your falvor')
 streamlit.text('Aalo Parantha with Pudina Chutney')
