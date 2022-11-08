8c5c49ede867c80773a846c119010df23d49931c
Vipin Vats <117748955+vipvats131@users.noreply.github.com>
2022-11-08 15:34:23 -0500

    Update streamlit_app.py
    Added Fruit List for smoothie's

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.text('\N{bread} \N{butter} \N{hot beverage} Toasted Bread with Butter 
 streamlit.text('\N{bowl with spoon} Cereal with choice of your falvor')
 streamlit.text('\N{flatbread} Aalo Parantha with Pudina Chutney')
 streamlit.header('\N{banana} \N{strawberry} Build your own smoothie \N{blueberries} \N{peach}')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
